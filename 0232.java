4c1235fcf25e4810f0bcc0730f789a0c7c5bb90d
Lcy1359 <Lcy1359@gmail.com>
2019-05-26 12:03:08 +0800

    0232 Implement Queue using Stacks

---
+++ 0232.java
@@ -0,0 +1,39 @@
+        for(int i = 1; i <= len; i++){
+            spareSt.push(st.pop());
+        }
+        if(spareSt.isEmpty()) return 0;
+        int temp = spareSt.pop();
+        len--;
+        if(spareSt.isEmpty()){
+            return temp;
+        }else{
+            front = spareSt.peek();
+            for(int i = 1; i <=len; i++){
+                 st.push(spareSt.pop());
+            } 
+            return temp;
+        }
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return front;   
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        if(len == 0)
+            return true;
+        else
+            return false;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
